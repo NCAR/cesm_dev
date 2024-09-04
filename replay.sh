19f0b881bd8521492377a7004259a2985f7e9576
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-09-04 12:47:35 -0600

    case files for b.e30_beta02.BLT1850.ne30_t232.110

---
+++ replay.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+# Created 2024-08-30 15:14:09
+
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta02.BLT1850.ne30_t232.110"
+
+/glade/work/hannay/cesm_tags/cesm3_0_beta02/cime/scripts/create_newcase --compset BLT1850 --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
+
+cd "${CASEDIR}"
+
+./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
+
+./case.setup
+
+./preview_namelists
+
+./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
+
+./xmlchange RUN_REFDATE=0201-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./xmlchange MOM6_VERTICAL_GRID=hycom1
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange CHARGE_ACCOUNT=CESM0023
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+
+./case.submit
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./case.submit
+
