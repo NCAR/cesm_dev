ce069e9aacc848086fa9e426f6cc717f05a91437
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 15:22:33 -0600

    case files for b.e30_beta05.BLT1850.ne30_t232_wgx3.128

--- replay.sh
+++ replay.sh
@@ -2,49 +2,51 @@
 
 set -e
 
-# Created 2025-03-06 20:59:17
+# Created 2025-02-18 15:34:17
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.127"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.128"
 
-/glade/work/gmarques/cesm.sandboxes/cesm3_0_beta05/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case b.e30_beta05.BLT1850.ne30_t232_wgx3.127 --run-unsupported --project CESM0023
+/glade/work/hannay/cesm_tags/cesm3_0_beta05/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
 ./case.setup
 
-./case.setup --reset
+./preview_namelists
+
+./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
+
+./xmlchange RUN_REFDATE=0201-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./preview_namelists
 
 ./case.build
 
-./xmlchange CONTINUE_RUN=True
+./preview_namelists
 
-./xmlchange RESUBMIT=4
+./preview_namelists
 
-./check_case
+./case.build
 
-./xmlchange STOP_OPTION=nyears,STOP_N=4
+./xmlchange JOB_PRIORITY=regular
 
-./case.submit
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
 
-./case.submit
+./xmlchange REST_OPTION=nyears,REST_N=1
 
-./case.submit
-
-./xmlchange STOP_N=2
-
-./xmlchange RESUBMIT=8
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
 
 ./case.submit
 
 ./case.submit
 
-./xmlchange JOB_WALLCLOCK_TIME=01:30 --subgroup case.st_archive
-
-./xmlchange STOP_N=3
-
 ./case.submit
-
-./xmlchange JOB_WALLCLOCK_TIME=01:30:00 --subgroup case.st_archive
-
-./case.submit --job case.st_archive
 
