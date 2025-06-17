b878bbee56abc0b18ab4996cc949ecb062ba218b
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 14:24:19 -0600

    case files for b.e30_beta04.BLT1850.ne30_t232_wgx3.121_1pctco2

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-01-22 15:19:55
+# Created 2025-01-22 15:56:51
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta04.BLT1850.ne30_t232_wgx3.121_4xco2"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta04.BLT1850.ne30_t232_wgx3.121_1pctco2"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -16,9 +16,7 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
-
-./xmlchange RUN_REFDATE=0201-01-01
+./xmlchange RUN_REFDATE=0125-01-01
 
 ./xmlchange RUN_TYPE=hybrid
 
@@ -26,17 +24,9 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
-./preview_namelists
+./xmlchange MOM6_VERTICAL_GRID=hycom1
 
 ./preview_namelists
-
-./case.build
-
-./case.build
-
-./case.build
-
-./case.build
 
 ./xmlchange RUN_REFCASE=b.e30_beta04.BLT1850.ne30_t232_wgx3.121
 
@@ -56,7 +46,7 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=75,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
@@ -64,9 +54,39 @@ cd "${CASEDIR}"
 
 ./case.submit
 
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+
 ./case.submit
 
-./case.setup --reset
+./case.build
+
+./xmlchange RUN_REFCASE=b.e30_beta04.BLT1850.ne30_t232_wgx3.121
+
+./xmlchange RUN_REFDATE=0125-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+
+./case.submit
+
+./preview_namelists
+
+./case.build
+
+./case.submit
 
 ./case.setup --reset
 
@@ -76,7 +96,7 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=75,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=40,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
@@ -84,7 +104,7 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./xmlchange STOP_N=6
+./xmlchange STOP_N=4
 
 ./case.submit
 
