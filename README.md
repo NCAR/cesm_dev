b9b1d8d727c19b8f2b7589d45cbf6cfe0e58a41b
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 13:55:08 -0600

    case files for b.e30_beta04.BLT1850.ne30_t232_wgx3.121_4xco2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: b.e30_beta04.BLTHIST.ne30_t232_wgx3.121
-https://github.com/NCAR/cesm_dev/issues/34
+# case: b.e30_beta04.BLT1850.ne30_t232_wgx3.121_4xco2
+https://github.com/NCAR/cesm_dev/issues/36
--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2025-01-07 16:44:48
+# Created 2025-01-22 15:19:55
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta04.BLTHIST.ne30_t232_wgx3.121"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta04.BLT1850.ne30_t232_wgx3.121_4xco2"
 
-/glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset HIST_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
@@ -16,9 +16,31 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
+./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
+
+./xmlchange RUN_REFDATE=0201-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
+./case.build
+
+./case.build
+
+./case.build
+
 ./xmlchange RUN_REFCASE=b.e30_beta04.BLT1850.ne30_t232_wgx3.121
 
-./xmlchange RUN_REFDATE=0075-01-01
+./xmlchange RUN_REFDATE=0125-01-01
 
 ./xmlchange RUN_TYPE=hybrid
 
@@ -34,15 +56,7 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=75,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
@@ -50,23 +64,9 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./xmlchange RESUBMIT=50
-
-./xmlchange RESUBMIT=30
-
 ./case.submit
 
 ./case.setup --reset
-
-./case.setup --reset
-
-./case.build --clean
-
-./case.build
-
-./case.submit
-
-./case.submit
 
 ./case.setup --reset
 
@@ -84,9 +84,9 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./case.submit
-
-./xmlchange STOP_N=1
+./xmlchange STOP_N=6
 
 ./case.submit
+
+./xmlchange RESUBMIT=0
 
