5885e63529596c3b4bfdd4fc18fba353bf222d6b
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 15:29:36 -0600

    case files for b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.134

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.133
-https://github.com/NCAR/cesm_dev/issues/52
+# case: b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.134
+https://github.com/NCAR/cesm_dev/issues/53
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-03-11 20:44:35
+# Created 2025-03-12 14:00:39
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.133"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.134"
 
 /glade/work/hannay/cesm_tags/cesm3_0_alpha06b/cime/scripts/create_newcase --compset B1850C_LTso --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -14,26 +14,6 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
-
-./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.130
-
-./xmlchange RUN_REFDATE=0045-01-01
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
-
 ./xmlchange RUN_REFCASE=b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.130
 
 ./xmlchange RUN_REFDATE=0045-01-01
@@ -45,44 +25,6 @@ cd "${CASEDIR}"
 ./xmlchange RUN_REFDIR=cesm2_init
 
 ./preview_namelists
-
-./xmlchange RUN_REFCASE=b.e30_alpha06b.B1850C_LTso.ne30_t232_wgx3.130
-
-./xmlchange RUN_REFDATE=0045-01-01
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./case.build
-
-./xmlchange JOB_PRIORITY=regular
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./case.submit
-
-./case.setup
-
-./case.setup --reset
-
-./case.build --clean
 
 ./preview_namelists
 
@@ -103,10 +45,6 @@ cd "${CASEDIR}"
 ./case.submit
 
 ./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=0
 
 ./xmlchange RESUBMIT=0
 
