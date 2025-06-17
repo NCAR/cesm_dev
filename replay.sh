1eeaf93a3332f6edf53bcddee513b3c842e7bf64
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 14:32:53 -0600

    case files for b.e30_beta05.BLT1850.ne30_t232_wgx3.124

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-02-06 16:12:35
+# Created 2025-02-13 09:30:16
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.123"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.124"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta05/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -32,7 +32,29 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+
+./xmlchange RUN_REFCASE=b.e30_beta05.BLT1850.ne30_t232_wgx3.122
+
+./xmlchange RUN_REFDATE=0029-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
@@ -42,45 +64,19 @@ cd "${CASEDIR}"
 
 ./case.setup --reset
 
-./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
-
-./xmlchange RUN_REFDATE=0201-01-01
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./case.build --clean-all
-
-./case.build
-
-./case.build
-
-./case.build
-
-./case.build
-
-./case.build
+./case.build --clean
 
 ./preview_namelists
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./preview_namelists
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./case.submit
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
-
-./case.submit
-
-./preview_namelists
 
 ./case.submit
 
