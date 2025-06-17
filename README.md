dd84357745355b08e1c60119d73f6090758cacb7
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-17 15:23:35 -0600

    case files for b.e30_beta05.BLT1850.ne30_t232_wgx3.129

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: b.e30_beta05.BLT1850.ne30_t232_wgx3.128
-https://github.com/NCAR/cesm_dev/issues/46
+# case: b.e30_beta05.BLT1850.ne30_t232_wgx3.129
+https://github.com/NCAR/cesm_dev/issues/50
--- replay.sh
+++ replay.sh
@@ -2,51 +2,43 @@
 
 set -e
 
-# Created 2025-02-18 15:34:17
+# Created 2025-02-26 06:29:49
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.128"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_beta05.BLT1850.ne30_t232_wgx3.129"
 
-/glade/work/hannay/cesm_tags/cesm3_0_beta05/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/gmarques/cesm.sandboxes/cesm3_0_beta05_v2/cime/scripts/create_newcase --compset 1850_CAM70%LT_CLM60%BGC-CROP_CICE_MOM6_MOSART_DGLC%NOEVOLVE_WW3_SESP --res ne30pg3_t232_wg37 --case b.e30_beta05.BLT1850.ne30_t232_wgx3.129 --run-unsupported --project CESM0023
 
 cd "${CASEDIR}"
 
 ./case.setup
 
-./preview_namelists
-
-./xmlchange RUN_REFCASE=b.e23_alpha17f.BLT1850.ne30_t232.098
-
-./xmlchange RUN_REFDATE=0201-01-01
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
-
-./preview_namelists
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
 
 ./case.build
 
-./preview_namelists
+./xmlchange STOP_N=4,STOP_OPTION=nyears,RESUBMIT=9
 
-./preview_namelists
+./pelayout
+
+./case.setup --reset
+
+./case.build --clean-all
+
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
+
+./pelayout
 
 ./case.build
 
-./xmlchange JOB_PRIORITY=regular
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=4,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+./check_case
 
 ./case.submit
 
 ./case.submit
 
-./case.submit
+./xmlchange RESUBMIT=10
+
+./xmlchange RESUBMIT=20
+
+./xmlchange RESUBMIT=0
 
