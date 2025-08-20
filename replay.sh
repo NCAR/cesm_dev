0077071e6a3c5e9cdfaa1864ea7e446cd5f20795
Gustavo Marques <gmarques@ucar.edu>
2025-08-20 08:33:03 -0600

    local git repository created

---
+++ replay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Created 2025-08-20 08:30:28
+
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_alpha07b_dev.B1850C_LTso.ne30_t232_wgx3.195"
+
+/glade/work/gmarques/cesm.sandboxes/cesm3_0_alpha07b_dev/cime/scripts/create_newcase --compset B1850C_LTso --res ne30pg3_t232_wg37 --case b.e30_alpha07b_dev.B1850C_LTso.ne30_t232_wgx3.195 --run-unsupported --project cesm0023
+
+cd "${CASEDIR}"
+
+./case.setup
+
