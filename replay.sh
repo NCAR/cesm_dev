d81d7920283320534a7b93b88fafa4349af10a39
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-12-16 21:02:25 -0700

    no message provided

--- replay.sh
+++ replay.sh
@@ -32,3 +32,17 @@ cd "${CASEDIR}"
 
 ./case.build
 
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
