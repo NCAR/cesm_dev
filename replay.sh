45095ba64dd9503ffd2ec78fe8fe372874f1d4b9
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-02-18 12:10:25 -0700

    no message provided

--- replay.sh
+++ replay.sh
@@ -16,3 +16,17 @@ cd "${CASEDIR}"
 
 ./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
 
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
