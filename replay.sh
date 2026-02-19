126ed4445cc7cdda8402766941b750f52464fc3d
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-02-19 14:42:53 -0700

    <command> ./xmlchange ROF2OCN_LIQ_RMAPNAME=/glade/work/gmarques/cesm/tx2_3/runoff_mapping/map_r05_to_tx2_3_nnsm_e100r100_250327.nc  </command>

--- replay.sh
+++ replay.sh
@@ -28,3 +28,5 @@ cd "${CASEDIR}"
 
 ./xmlchange ROF2OCN_ICE_RMAPNAME=/glade/u/home/igrooms/runoff_mapping/map_r05_to_tx2_3_nnsm_e250r250_modified_260212.nc
 
+./xmlchange ROF2OCN_LIQ_RMAPNAME=/glade/work/gmarques/cesm/tx2_3/runoff_mapping/map_r05_to_tx2_3_nnsm_e100r100_250327.nc
+
