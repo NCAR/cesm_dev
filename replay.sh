3023084d364caecae29f995756dc2c4ff5539dc6
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-03-04 13:00:03 -0700

    no message provided

--- replay.sh
+++ replay.sh
@@ -48,3 +48,13 @@ cd "${CASEDIR}"
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
 
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
