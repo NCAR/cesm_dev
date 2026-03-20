6d51cc3235bb41d878075159c92b5b295fa3a3ff
Gustavo Marques <gmarques@ucar.edu>
2026-03-20 15:51:41 -0600

    <command> ./xmlchange OCN_DOMAIN_MESH=/glade/work/gmarques/cesm/tx2_3/mesh/ESMF_mesh_tx2_3v3_260305_cdf5.nc  </command>

--- replay.sh
+++ replay.sh
@@ -16,3 +16,5 @@ cd "${CASEDIR}"
 
 ./xmlchange MASK_MESH=/glade/work/gmarques/cesm/tx2_3/mesh/ESMF_mesh_tx2_3v3_260305_cdf5.n
 
+./xmlchange OCN_DOMAIN_MESH=/glade/work/gmarques/cesm/tx2_3/mesh/ESMF_mesh_tx2_3v3_260305_cdf5.nc
+
