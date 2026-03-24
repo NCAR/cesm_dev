d5e922ea4f02d417b11e10623c0f4c6ea61e0aa3
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-03-24 14:39:36 -0600

    local git repository created

---
+++ replay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Created 2026-03-24 14:39:30
+
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_alpha08m.B1850C_LTso.ne30_t232_wgx3.329_test2"
+
+/glade/work/hannay/cesm_tags/cesm3_0_alpha08m/cime/scripts/create_newcase --compset B1850C_LTso --res ne30pg3_t232_wg37 --case "${CASEDIR}" --run-unsupported --project 93300722
+
+cd "${CASEDIR}"
+
+./case.setup
+
