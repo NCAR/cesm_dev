c9fc054c249f3db0f097613005175824d8768075
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-03-27 15:40:49 -0600

    no message provided

---
+++ cmake_macros/CMakeFiles/3.31.8/CMakeFortranCompiler.cmake
@@ -0,0 +1,70 @@
+set(CMAKE_Fortran_COMPILER "/glade/u/apps/derecho/25.10/spack/opt/spack/ncarcompilers/1.2.0/intel-oneapi-compilers/2025.3.2/u3vq/bin/ifx")
+set(CMAKE_Fortran_COMPILER_ARG1 "")
+set(CMAKE_Fortran_COMPILER_ID "IntelLLVM")
+set(CMAKE_Fortran_COMPILER_VERSION "2025.3.2")
+set(CMAKE_Fortran_COMPILER_WRAPPER "CrayPrgEnv")
+set(CMAKE_Fortran_PLATFORM_ID "Linux")
+set(CMAKE_Fortran_SIMULATE_ID "")
+set(CMAKE_Fortran_COMPILER_FRONTEND_VARIANT "GNU")
+set(CMAKE_Fortran_SIMULATE_VERSION "")
+
+
+
+
+set(CMAKE_AR "/usr/bin/ar")
+set(CMAKE_Fortran_COMPILER_AR "CMAKE_Fortran_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "/usr/bin/ranlib")
+set(CMAKE_LINKER "/usr/bin/ld")
+set(CMAKE_Fortran_COMPILER_LINKER "ld")
+set(CMAKE_Fortran_COMPILER_LINKER_ID "GNU")
+set(CMAKE_Fortran_COMPILER_LINKER_VERSION 2.43.1.20241209)
+set(CMAKE_Fortran_COMPILER_LINKER_FRONTEND_VARIANT GNU)
+set(CMAKE_Fortran_COMPILER_RANLIB "CMAKE_Fortran_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
+set(CMAKE_COMPILER_IS_GNUG77 )
+set(CMAKE_Fortran_COMPILER_LOADED 1)
+set(CMAKE_Fortran_COMPILER_WORKS TRUE)
+set(CMAKE_Fortran_ABI_COMPILED TRUE)
+
+set(CMAKE_Fortran_COMPILER_ENV_VAR "FC")
+
+set(CMAKE_Fortran_COMPILER_SUPPORTS_F90 1)
+
+set(CMAKE_Fortran_COMPILER_ID_RUN 1)
+set(CMAKE_Fortran_SOURCE_FILE_EXTENSIONS f;F;fpp;FPP;f77;F77;f90;F90;for;For;FOR;f95;F95;f03;F03;f08;F08)
+set(CMAKE_Fortran_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_Fortran_LINKER_PREFERENCE 20)
+set(CMAKE_Fortran_LINKER_DEPFILE_SUPPORTED TRUE)
+set(CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+set(CMAKE_Fortran_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+if(UNIX)
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .o)
+else()
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .obj)
+endif()
+
+# Save compiler ABI information.
+set(CMAKE_Fortran_SIZEOF_DATA_PTR "8")
+set(CMAKE_Fortran_COMPILER_ABI "ELF")
+set(CMAKE_Fortran_LIBRARY_ARCHITECTURE "x86_64-unknown-linux-gnu")
+
+if(CMAKE_Fortran_SIZEOF_DATA_PTR AND NOT CMAKE_SIZEOF_VOID_P)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_Fortran_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_Fortran_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_Fortran_COMPILER_ABI}")
+endif()
+
+if(CMAKE_Fortran_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "x86_64-unknown-linux-gnu")
+endif()
+
+
+
+
+
+set(CMAKE_Fortran_IMPLICIT_INCLUDE_DIRECTORIES "/glade/u/apps/derecho/25.10/spack/opt/spack/parallel-netcdf/1.14.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/wlvp/include;/glade/u/apps/derecho/25.10/spack/opt/spack/parallelio/2.6.8/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/q7o5/include;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/include;/opt/cray/libfabric/1.22.0/include;/glade/u/apps/derecho/25.10/spack/opt/spack/hdf5/1.14.6/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/jzti/include;/glade/u/apps/derecho/25.10/spack/opt/spack/esmf/8.9.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/uh76/include;/glade/u/apps/derecho/25.10/opt/view/include;/opt/cray/pe/mpich/8.1.32/ofi/intel/2022.1/include;/opt/cray/pe/pmi/6.1.15/include;/opt/cray/pals/1.6/include;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/umf/1.0/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/opt/compiler/include/intel64;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/opt/compiler/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/include;/usr/local/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib/clang/21/include;/usr/include")
+set(CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES "pmi;pmi2;pthread;pals;mpifort_intel;mpi_intel;rt;pthread;pmi;pmi2;imf;m;pthread;ifport;ifcoremt;imf;svml;m;ipgo;irc;pthread;svml;c;gcc;gcc_s;irc_s;dl;c")
+set(CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES "/glade/u/apps/derecho/25.10/spack/opt/spack/parallel-netcdf/1.14.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/wlvp/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/parallelio/2.6.8/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/q7o5/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/lib64;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/nco/5.3.4/gcc/12.5.0/chl2/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3.1/lib/intel64;/opt/cray/libfabric/1.22.0/lib64;/glade/u/apps/derecho/25.10/spack/opt/spack/hdf5/1.14.6/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/jzti/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/esmf/8.9.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/uh76/lib;/glade/u/apps/derecho/25.10/opt/view/lib64;/glade/u/apps/derecho/25.10/opt/view/lib;/opt/cray/pe/mpich/8.1.32/ofi/intel/2022.1/lib;/opt/cray/pe/pmi/6.1.15/lib;/opt/cray/pals/1.6/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3/lib;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/umf/1.0/lib;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib64;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib/clang/21/lib/x86_64-unknown-linux-gnu;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib/gcc/x86_64-pc-linux-gnu/12.5.0;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib/gcc;/lib64;/usr/lib64;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib;/lib;/usr/lib")
+set(CMAKE_Fortran_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
