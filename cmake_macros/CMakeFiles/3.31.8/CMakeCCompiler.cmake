627d45b5396835e3be7a058ea6186155d9a5791f
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-04-14 15:28:32 -0600

    no message provided

---
+++ cmake_macros/CMakeFiles/3.31.8/CMakeCCompiler.cmake
@@ -0,0 +1,83 @@
+set(CMAKE_C_COMPILER "/glade/u/apps/derecho/25.10/spack/opt/spack/ncarcompilers/1.2.0/intel-oneapi-compilers/2025.3.2/u3vq/bin/icx")
+set(CMAKE_C_COMPILER_ARG1 "")
+set(CMAKE_C_COMPILER_ID "IntelLLVM")
+set(CMAKE_C_COMPILER_VERSION "2025.3.2")
+set(CMAKE_C_COMPILER_VERSION_INTERNAL "")
+set(CMAKE_C_COMPILER_WRAPPER "CrayPrgEnv")
+set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "17")
+set(CMAKE_C_EXTENSIONS_COMPUTED_DEFAULT "ON")
+set(CMAKE_C_STANDARD_LATEST "23")
+set(CMAKE_C_COMPILE_FEATURES "c_std_90;c_function_prototypes;c_std_99;c_restrict;c_variadic_macros;c_std_11;c_static_assert;c_std_17;c_std_23")
+set(CMAKE_C90_COMPILE_FEATURES "c_std_90;c_function_prototypes")
+set(CMAKE_C99_COMPILE_FEATURES "c_std_99;c_restrict;c_variadic_macros")
+set(CMAKE_C11_COMPILE_FEATURES "c_std_11;c_static_assert")
+set(CMAKE_C17_COMPILE_FEATURES "c_std_17")
+set(CMAKE_C23_COMPILE_FEATURES "c_std_23")
+
+set(CMAKE_C_PLATFORM_ID "Linux")
+set(CMAKE_C_SIMULATE_ID "GNU")
+set(CMAKE_C_COMPILER_FRONTEND_VARIANT "GNU")
+set(CMAKE_C_SIMULATE_VERSION "4.2.1")
+
+
+
+
+set(CMAKE_AR "/usr/bin/ar")
+set(CMAKE_C_COMPILER_AR "CMAKE_C_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "/usr/bin/ranlib")
+set(CMAKE_C_COMPILER_RANLIB "CMAKE_C_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_LINKER "/usr/bin/ld")
+set(CMAKE_LINKER_LINK "")
+set(CMAKE_LINKER_LLD "")
+set(CMAKE_C_COMPILER_LINKER "/usr/bin/ld")
+set(CMAKE_C_COMPILER_LINKER_ID "GNU")
+set(CMAKE_C_COMPILER_LINKER_VERSION 2.43.1.20241209)
+set(CMAKE_C_COMPILER_LINKER_FRONTEND_VARIANT GNU)
+set(CMAKE_MT "")
+set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
+set(CMAKE_COMPILER_IS_GNUCC )
+set(CMAKE_C_COMPILER_LOADED 1)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_C_ABI_COMPILED TRUE)
+
+set(CMAKE_C_COMPILER_ENV_VAR "CC")
+
+set(CMAKE_C_COMPILER_ID_RUN 1)
+set(CMAKE_C_SOURCE_FILE_EXTENSIONS c;m)
+set(CMAKE_C_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_C_LINKER_PREFERENCE 10)
+set(CMAKE_C_LINKER_DEPFILE_SUPPORTED TRUE)
+set(CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+set(CMAKE_C_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+
+# Save compiler ABI information.
+set(CMAKE_C_SIZEOF_DATA_PTR "8")
+set(CMAKE_C_COMPILER_ABI "ELF")
+set(CMAKE_C_BYTE_ORDER "LITTLE_ENDIAN")
+set(CMAKE_C_LIBRARY_ARCHITECTURE "x86_64-unknown-linux-gnu")
+
+if(CMAKE_C_SIZEOF_DATA_PTR)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_C_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_C_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_C_COMPILER_ABI}")
+endif()
+
+if(CMAKE_C_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "x86_64-unknown-linux-gnu")
+endif()
+
+set(CMAKE_C_CL_SHOWINCLUDES_PREFIX "")
+if(CMAKE_C_CL_SHOWINCLUDES_PREFIX)
+  set(CMAKE_CL_SHOWINCLUDES_PREFIX "${CMAKE_C_CL_SHOWINCLUDES_PREFIX}")
+endif()
+
+
+
+
+
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "/glade/u/apps/derecho/25.10/spack/opt/spack/parallel-netcdf/1.14.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/wlvp/include;/glade/u/apps/derecho/25.10/spack/opt/spack/parallelio/2.6.8/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/q7o5/include;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/include;/opt/cray/libfabric/1.22.0/include;/glade/u/apps/derecho/25.10/spack/opt/spack/hdf5/1.14.6/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/jzti/include;/glade/u/apps/derecho/25.10/spack/opt/spack/esmf/8.9.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/uh76/include;/glade/u/apps/derecho/25.10/opt/view/include;/opt/cray/pe/mpich/8.1.32/ofi/intel/2022.1/include;/opt/cray/pe/pmi/6.1.15/include;/opt/cray/pals/1.6/include;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/umf/1.0/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/opt/compiler/include;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib/clang/21/include;/usr/local/include;/usr/include")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "pmi;pmi2;pthread;pals;mpi_intel;rt;pthread;pmi;pmi2;imf;m;ifcoremt;ifport;pthread;svml;irng;imf;m;gcc;gcc_s;irc;dl;gcc;gcc_s;c;gcc;gcc_s;irc_s")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/glade/u/apps/derecho/25.10/spack/opt/spack/parallel-netcdf/1.14.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/wlvp/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/parallelio/2.6.8/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/q7o5/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/lib64;/glade/u/apps/derecho/25.10/spack/opt/spack/netcdf/4.9.3/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/yqys/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/nco/5.3.4/gcc/12.5.0/chl2/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3.1/lib/intel64;/opt/cray/libfabric/1.22.0/lib64;/glade/u/apps/derecho/25.10/spack/opt/spack/hdf5/1.14.6/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/jzti/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/esmf/8.9.1/cray-mpich/8.1.32/intel-oneapi-compilers/2025.3.2/uh76/lib;/glade/u/apps/derecho/25.10/opt/view/lib64;/glade/u/apps/derecho/25.10/opt/view/lib;/opt/cray/pe/mpich/8.1.32/ofi/intel/2022.1/lib;/opt/cray/pe/pmi/6.1.15/lib;/opt/cray/pals/1.6/lib;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/lib/clang/21/lib/x86_64-unknown-linux-gnu;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib/gcc/x86_64-pc-linux-gnu/12.5.0;/glade/u/apps/common/25.10/spack/opt/spack/gcc/12.5.0/hsbg/lib64;/lib64;/usr/lib64;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/compiler/2025.3/opt/compiler/lib;/lib;/usr/lib;/glade/u/apps/derecho/25.10/spack/opt/spack/intel-oneapi-mkl/2025.3.1/intel-oneapi-compilers/2025.3.2/ydfo/mkl/2025.3/lib;/glade/u/apps/common/25.10/spack/opt/spack/intel-oneapi-compilers/2025.3.2/snvt/umf/1.0/lib")
+set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
