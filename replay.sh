dfcc4014126270e79b4ed4c4ef06d0bb40095b45
Cecile Hannay <cecilehannay@users.noreply.github.com>
2026-07-17 14:44:42 -0600

    local git repository created

---
+++ replay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Created 2026-07-17 14:44:39
+
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e30_alpha09d_m.B1850C_MTso_Gris_Marbl.ne30_t233_wgx3.371"
+
+/glade/work/hannay/cesm_tags/cesm3_0_alpha09e_MARBL/cime/scripts/create_newcase --compset 1850C_CAM70%MT_CLM60%BGC-CROP_CICE_MOM6%MARBL-BIO_MOSART_CISM2%GRIS-EVOLVE_WW3_SESP --res ne30pg3_t233_wg37_gris4 --case "${CASEDIR}" --run-unsupported --project 93300722
+
+cd "${CASEDIR}"
+
+./case.setup
+
