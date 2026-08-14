d35b77b1fc0d1e3e2ace7359dcb49b2c5e4ad7b2
David Bailey <dbailey@ucar.edu>
2026-08-14 13:53:37 -0600

    <command> ./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git  </command>

--- replay.sh
+++ replay.sh
@@ -20,3 +20,21 @@
 
 ./case.submit
 
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com:NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git
+
+./xmlchange CASE_GIT_REPOSITORY=https://github.com/NCAR/cesm_dev.git
+
