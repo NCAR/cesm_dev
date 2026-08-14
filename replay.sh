04eda8ec4bc8c1def8fbc630a6e2fb648bf960f0
Gustavo Marques <gmarques@ucar.edu>
2026-08-14 09:56:39 -0600

    <command> ./xmlchange RUN_REFCASE=b.e30_alpha08o.B1850C_MTso.ne30_t232_wgx3.341  </command>

--- replay.sh
+++ replay.sh
@@ -14,3 +14,7 @@ cd "${CASEDIR}"
 
 ./xmlchange CAM_CONFIG_OPTS="-pcols 9" --append
 
+./xmlchange CASE_GIT_REPOSITORY=git@github.com:NCAR/cesm_dev.git
+
+./xmlchange RUN_REFCASE=b.e30_alpha08o.B1850C_MTso.ne30_t232_wgx3.341
+
